2b75d05021f8a8141c4b617491b6d0bc2e35a5b1
Jhon Freddy Puentes Nuñez <j.puentesn@uniandes.edu.co>
2025-05-18 12:20:36 -0500

    fix pruebas e2e.

--- src/biblioteca-libro/biblioteca-libro.module.ts
+++ src/biblioteca-libro/biblioteca-libro.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BibliotecaLibroService } from './biblioteca-libro.service';
-import { Biblioteca } from 'src/biblioteca/biblioteca/biblioteca';
-import { Libro } from 'src/libro/libro/libro';
+import { Biblioteca } from '../biblioteca/biblioteca/biblioteca';
+import { Libro } from '../libro/libro/libro';
 import { BibliotecaLibroController } from './biblioteca-libro.controller';
 
 @Module({
